30ee5f185582dacb14da23f1fb837df4295fbfdb
Kate Christensen <katec125@gmail.com>
2025-08-04 01:15:41 -0500

    Quartz sync: Aug 4, 2025, 1:15 AM

--- quartz.config.ts
+++ quartz.config.ts
@@ -16,7 +16,7 @@ const config: QuartzConfig = {
       provider: "plausible",
     },
     locale: "en-US",
-    baseUrl: "quartz.jzhao.xyz",
+    baseUrl: "the-goblin-kate.nekoweb.org",
     ignorePatterns: ["private", "templates", ".obsidian"],
     defaultDateType: "modified",
     theme: {
